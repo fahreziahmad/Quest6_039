aecdfc6e8f9a300ea4270d32cfdba59922b2dfbb
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-21 17:24:27 +0700

    menambahkan fungsi FormIsian yang berfungsi sebagai form input dalam UI Jetpack Compose.

---
+++ app/src/main/java/com/example/quest6_039/view/Formulir.kt
@@ -0,0 +1,50 @@
+package com.example.quest6_039.view
+
+// 2. IMPORT YANG DIPERLUKAN DITAMBAHKAN
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.DividerDefaults.Thickness
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue // <-- Import baru
+import androidx.compose.runtime.mutableStateOf // <-- Import baru
+import androidx.compose.runtime.remember // <-- Import baru
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue // <-- Import baru
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.quest6_039.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    pilihanJK: List<String> ,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by rememberSaveable { mutableStateOf("") }
+    var txtGender by rememberSaveable { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
+
