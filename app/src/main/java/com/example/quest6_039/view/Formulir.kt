f2b114fa290b81ff123d104a4eab430642929055
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-21 17:27:12 +0700

    menambahkan elemen UI berupa divider dan komponen pilihan jenis kelamin menggunakan RadioButton dalam form input.

--- app/src/main/java/com/example/quest6_039/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_039/view/Formulir.kt
@@ -72,3 +72,27 @@ fun FormIsian(
                     txtNama = it
                 }
             )
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp), thickness = Thickness, color = Color.Red)
+            Row{
+                pilihanJK.forEach {
+                        item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                        verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+
+                            }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
