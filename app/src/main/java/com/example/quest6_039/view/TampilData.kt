11a90aeb8260ab210cdc16e4bb6f0d2ca7da40ea
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-21 17:30:20 +0700

    menambahkan fungsi TampilData yang digunakan untuk menampilkan informasi lengkap dari data siswa yang sudah diinput.

---
+++ app/src/main/java/com/example/quest6_039/view/TampilData.kt
@@ -0,0 +1,40 @@
+package com.example.quest6_039.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quest6_039.R
+import com.example.quest6_039.model.siswa
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    StatusUISiswa: siswa,
+    onBackButtonClicked: () -> Unit
+){
+    val items = listOf(
+        Pair(stringResource(R.string.nama), StatusUISiswa.nama),
+        Pair(stringResource(R.string.gender), StatusUISiswa.gender),
+        Pair(stringResource(R.string.alamat), StatusUISiswa.alamat)
+    )
